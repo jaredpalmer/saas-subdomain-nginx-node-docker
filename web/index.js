2282c012119f77fe827b58db00aa126c29fe67c4
Jared Palmer <jaredpalmer>
2016-08-23 11:40:44 -0400

    change ngnix server_name

--- web/index.js
+++ web/index.js
@@ -1,18 +1,17 @@
 const express = require('express')
 const http = require('http')
 const morgan = require('morgan')
-const proxy = require('http-proxy')
 
 const app = express()
 
 app.use(morgan('dev'))
 app.use(express.static('public'))
 app.get('/api', (req, res) => {
-  console.log(req.get('X-My-Custom-Param-1'));
   res.send('Hello world!')
 })
 app.get('/api/me', (req, res) => {
   const tenant = req.get('X-My-Custom-Param-1') || 'no tenant'
+  console.log(tenant)
   res.send(tenant)
 })
 
