b6697963e30e380bc5136502dd90ded0b65b9408
Jared Palmer <jaredpalmer>
2016-08-23 09:11:12 -0400

    init

---
+++ web/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const http = require('http')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.static('public'))
+app.get('/api', (req, res) => {
+  console.log(req.get('X-My-Custom-Param-1'));
+  res.send('Hello world!')
+})
+app.get('/api/me', (req, res) => {
+  const tenant = req.get('X-My-Custom-Param-1');
+  res.send(tenant)
+})
+
+const server = http.createServer(app)
+
+server.listen(3000, (err) => {
+  if (err) console.log(err)
+  console.log("server listening on port 3000")
+})
